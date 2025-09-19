ef9b38b1c55ab0f50a8ee629eb0b85baec826e85
sarah <skg@neofilm.us>
2025-09-19 14:52:30 -0400

    Add functionality so users can edit notes

--- src/Note.js
+++ src/Note.js
@@ -1,22 +1,45 @@
 import React from "react";
 
-const Note = (props) => (
-  <li className="note">
-    <input
-      type="text"
-      aria-label="Title"
-      placeholder="ToDo Today"
-      className="note__title"
-      value={props.note.title}
-    />
-    <textarea
-      aria-label="Description"
-      placeholder="Take a nap!"
-      className="note__description"
-      value={props.note.description}
-    />
-    <span className="note__delete">X</span>
-  </li>
-);
+/* This presentational component renders a single
+list item element with two text input fields,
+one for the note's title and one for the note's
+description. When the user types in these fields,
+functions that are passed to this component
+as props are used to update the state of the
+corresponding note object in the state of the
+App component  */
+const Note = (props) => {
+  const updateTitle = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "title", updatedValue);
+  };
+  const updateDescription = (e) => {
+    const updatedValue = e.target.value;
+    const editMeId = props.note.id;
+    props.onType(editMeId, "description", updatedValue);
+  };
+
+  return (
+    <li className="note">
+      <input
+        type="text"
+        aria-label="Title"
+        value={props.note.title}
+        onChange={updateTitle}
+        placeholder="ToDo Today"
+        className="note__title"
+      />
+      <textarea
+        aria-label="Description"
+        value={props.note.description}
+        onChange={updateDescription}
+        placeholder="Take a nap!"
+        className="note__description"
+      />
+      <span className="note__delete">X</span>
+    </li>
+  );
+};
 
 export default Note;
